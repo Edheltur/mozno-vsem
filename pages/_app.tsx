fe6726ec5b8fa82dcac3813bdd0c8b4c979e496e
edheltur <edheltur@yandex-team.ru>
2020-08-02 19:53:45 +0500

    Втащил Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
+import { AppProps } from "next/app";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
