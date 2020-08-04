18d6f630a990f1cd9078ad0af9b15b682efbb1ed
edheltur <edheltur@yandex-team.ru>
2020-08-04 15:05:21 +0500

    Фикс шрифтов

--- components/Favicons/Favicons.tsx
+++ components/Favicons/Favicons.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import React from "react";
 
 export function Favicons() {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { AppProps } from "next/app";
-import { store, AppStateContext } from "store";
+import { AppStateContext, store } from "store";
 
+import "styles/fonts.css";
 import "styles/globals.css";
 import "styles/semantic.css";
-import "styles/seamntic-deferred.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <AppStateContext.Provider value={store}>
