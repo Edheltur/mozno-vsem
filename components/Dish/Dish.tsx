e09137033e6bc863b40a9c1a0fbb1fbd7b50e5d7
edheltur <edheltur@yandex-team.ru>
2020-08-04 20:58:55 +0500

    Оптимизировал картинки

--- components/Dish/Dish.tsx
+++ components/Dish/Dish.tsx
@@ -9,25 +9,27 @@ interface IProps extends IMenuItem {
   mix?: string;
 }
 
-export const Dish = React.memo(({ title, weight, price, mix, id }: IProps) => {
-  const image = `/images/dishes/preview/${id}.jpeg`;
-  const { dispatch } = useAppState("cart");
+export const Dish = React.memo(
+  ({ title, weight, price, mix, id, previewImage }: IProps) => {
+    const image = `/images/dishes/preview/${previewImage}`;
+    const { dispatch } = useAppState("cart");
 
-  const handle = React.useMemo(
-    () => ({
-      click: () => dispatch("cart/add", { id }),
-    }),
-    [dispatch, id]
-  );
+    const handle = React.useMemo(
+      () => ({
+        click: () => dispatch("cart/add", { id }),
+      }),
+      [dispatch, id]
+    );
 
-  return (
-    <div className={cn(styles.Dish, mix)} onClick={handle.click}>
-      <img className={styles.Dish__image} src={image} alt="" />
-      <div className={styles.Dish__title}>{title}</div>
-      <div className={styles.Dish__weight}>{weight}&nbsp;г</div>
-      <Button className={styles.Dish__price} size="mini">
-        {price}&nbsp;₽
-      </Button>
-    </div>
-  );
-});
+    return (
+      <div className={cn(styles.Dish, mix)} onClick={handle.click}>
+        <img className={styles.Dish__image} src={image} alt="" />
+        <div className={styles.Dish__title}>{title}</div>
+        <div className={styles.Dish__weight}>{weight}&nbsp;г</div>
+        <Button className={styles.Dish__price} size="mini">
+          {price}&nbsp;₽
+        </Button>
+      </div>
+    );
+  }
+);
--- data/menu.ts
+++ data/menu.ts
@@ -5,6 +5,7 @@ export interface IMenuItem {
   title: string;
   price: number;
   weight: number;
+  previewImage: string;
 }
 
 export const itemsBydId = {
@@ -13,54 +14,70 @@ export const itemsBydId = {
     title: "Гречаники",
     price: 280,
     weight: 400,
+    previewImage: "grechaniki.jpg",
   },
   2: {
     id: 2,
     title: "Котлеты из телятины и булгура",
     price: 440,
     weight: 400,
+    previewImage: "tefteli-so-shpinatom.jpg",
   },
   3: {
     id: 3,
     title: "Котлеты из телятины с кабачком и зеленью",
     price: 340,
     weight: 300,
+    previewImage: "kotlety-iz-telyatiny-s-kabachkom-i-zelenyu.jpg",
   },
   4: {
     id: 4,
     title: "Фрикадельки из индейки с луком",
     price: 410,
     weight: 480,
+    previewImage: "grechaniki.jpg",
   },
   5: {
     id: 5,
     title: "Фрикадельки из индейки со шпинатом",
     price: 420,
     weight: 480,
+    previewImage: "tefteli-so-shpinatom.jpg",
   },
   6: {
     id: 6,
     title: "Биточки из трески",
     price: 350,
     weight: 300,
+    previewImage: "kotlety-iz-telyatiny-s-kabachkom-i-zelenyu.jpg",
   },
   7: {
     id: 7,
     title: "Котлеты из курицы с имбирём",
     price: 440,
     weight: 520,
+    previewImage: "grechaniki.jpg",
   },
   8: {
     id: 8,
     title: "Ленивые голубцы",
     price: 220,
     weight: 300,
+    previewImage: "tefteli-so-shpinatom.jpg",
   },
   9: {
     id: 9,
     title: "Перец фаршированный",
     price: 350,
     weight: 400,
+    previewImage: "grechaniki.jpg",
+  },
+  10: {
+    id: 10,
+    title: "Тефтели со шпинатом",
+    price: 440,
+    weight: 400,
+    previewImage: "tefteli-so-shpinatom.jpg",
   },
 } as const;
 
