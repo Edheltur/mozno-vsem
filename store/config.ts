5371517fe969a5d6cf3ca70561023de7a1ef14b8
edheltur <edheltur@yandex-team.ru>
2020-08-07 14:07:47 +0500

    Фикс опечатки

--- store/config.ts
+++ store/config.ts
@@ -4,7 +4,7 @@ import { Events, State } from "store/types";
 export const config: StoreonModule<State, Events> = (store) => {
   store.on("@init", () => ({
     config: {
-      telegramUsername: process.env.NEXT_PUBLIC_TELEGRAMM_USER_NAME ?? "",
+      telegramUsername: process.env.NEXT_PUBLIC_TELEGRAM_USER_NAME ?? "",
       whatsAppPhoneNumber: process.env.NEXT_PUBLIC_WHATS_APP_PHONE_NUMBER ?? "",
     },
   }));
