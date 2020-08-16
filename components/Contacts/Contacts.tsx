20908a9934c671d00d2af15608db378c70a70f7b
edheltur <edheltur@yandex-team.ru>
2020-08-16 18:40:59 +0500

    Поправил текст

--- components/Contacts/Contacts.tsx
+++ components/Contacts/Contacts.tsx
@@ -5,7 +5,7 @@ import { Anchor } from "grommet";
 export const Contacts = () => (
   <InfoBlock title="Кто мы?">
     Мы занимается приготовлением и&nbsp;доставкой полезных полуфабрикатов.
-    Не&nbsp;используем вредные пищевые добавки, только натуральные специи
+    Не&nbsp;используем вредные добавки, только натуральные специи
     и&nbsp;пряности. А&nbsp;ещё у&nbsp;нас есть{" "}
     <Anchor href="https://www.instagram.com/mozno_vsem">Instagram</Anchor>.
   </InfoBlock>
