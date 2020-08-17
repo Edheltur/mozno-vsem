175a68cb2173537d9c4f185f389c90a36fd94e5f
edheltur <edheltur@yandex-team.ru>
2020-08-17 17:55:53 +0500

    Фикс пробела

--- components/Contacts/Contacts.tsx
+++ components/Contacts/Contacts.tsx
@@ -6,7 +6,7 @@ export const Contacts = () => (
   <InfoBlock title="Кто мы?">
     Мы занимаемся приготовлением и&nbsp;доставкой полезных полуфабрикатов.
     Не&nbsp;используем вредные добавки, только натуральные специи
-    и&nbsp;пряности. А&nbsp;ещё у&nbsp;нас есть
+    и&nbsp;пряности. А&nbsp;ещё у&nbsp;нас есть{" "}
     <Anchor href="https://www.instagram.com/mozno_vsem">Instagram</Anchor>.
   </InfoBlock>
 );
