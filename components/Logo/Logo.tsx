6bff732f1b16f682cfb00a59e3d4bd35d60fc7ea
edheltur <edheltur@yandex-team.ru>
2020-08-23 21:24:41 +0500

    Убрал лишний мем

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import cn from "classnames";
 
 import styles from "./Logo.module.css";
 
-interface IProps {
-  mix?: string;
-}
-
-export const Logo = React.memo(function Logo({ mix }: IProps) {
-  return <div className={cn(styles.Logo, mix)} />;
-});
+export const Logo = () => <div className={styles.Logo} />;
