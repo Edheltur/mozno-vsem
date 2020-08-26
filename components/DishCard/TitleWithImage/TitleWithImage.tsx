0af130259dd9f94768eae3b32479ae0a6000e849
edheltur <edheltur@yandex-team.ru>
2020-08-26 10:35:40 +0500

    Добавил размеры картинкам

--- components/DishCard/TitleWithImage/TitleWithImage.tsx
+++ components/DishCard/TitleWithImage/TitleWithImage.tsx
@@ -17,11 +17,7 @@ export const TitleWithImage = React.memo(function TitleWithImage({
   return (
     <Box flex="grow">
       <Link href="/dish/[id]" as={`/dish/${item.id}`}>
-        <Image
-          className={styles.TitleWithImage__image}
-          src={imageUrl}
-          alt={title}
-        />
+        <Image width={150} height={150} src={imageUrl} alt={title} />
       </Link>
       <Text size="15px" textAlign="center">
         <Link passHref href="/dish/[id]" as={`/dish/${item.id}`}>
