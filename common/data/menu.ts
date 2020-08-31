26ced9c060d50906eec884df94212af99880b2d2
edheltur <edheltur@yandex-team.ru>
2020-08-31 12:38:33 +0500

    Убрал фарши по 1000кг

--- common/data/menu.ts
+++ common/data/menu.ts
@@ -294,8 +294,7 @@ const rawItemsById = {
 // prettier-ignore
 const orderedIds: ReadonlyArray<TMenuItemId> = [
   "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-  "11", "22", "12", "13", "14", "15", "16", "17", "18",
-  "19",  "20", "21"
+  "11", "22", "13", "15", "17", "19", "21"
 ];
 
 export const itemsBydId: Readonly<Record<
