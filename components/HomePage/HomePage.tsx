d07725f0967048b7c5018c311e0ee8bf9f369c72
Arkady Anderjushkin <edheltur@yandex.ru>
2020-12-06 02:47:40 +0500

    Убрал смену title

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import Head from "next/head";
 
 import { DishList } from "components/DishList";
 import { DeliveryInfo } from "components/DeliveryInfo";
-import { useRouter } from "next/router";
-import { categories } from "common/data/categories";
 
 export const HomePage = () => {
-  const router = useRouter();
-  const { category: categorySlug } = router.query;
-
-  const category = categories.find((x) => x.slug === categorySlug);
   return (
     <>
-      {category && (
-        <Head>
-          <title>Можно всем – {category.title}</title>
-        </Head>
-      )}
       <DeliveryInfo />
       <DishList />
     </>
