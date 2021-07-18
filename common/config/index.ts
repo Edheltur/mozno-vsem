d34302704171a4d12c6aa78ef0c5fdf3f0855f2d
Arkady Anderjushkin <edheltur@yandex.ru>
2021-07-18 15:40:01 +0500

    Поменял условия доставки

--- common/config/index.ts
+++ common/config/index.ts
@@ -2,6 +2,7 @@ export const config = {
   publicUrl: "https://mozno-vsem.ru",
   freeDeliveryFrom: 2500,
   deliveryPrice: 300,
+  farDeliveryPrice: 300,
   telegramUsername: process.env.NEXT_PUBLIC_TELEGRAM_USER_NAME ?? "",
   whatsAppPhoneNumber: process.env.NEXT_PUBLIC_WHATS_APP_PHONE_NUMBER ?? "",
   yandexMetrikaCounterId: process.env.NEXT_PUBLIC_YANDEX_METRIKA_COUNTER,
--- components/DeliveryInfo/DeliveryInfo.tsx
+++ components/DeliveryInfo/DeliveryInfo.tsx
@@ -6,7 +6,7 @@ export const DeliveryInfo = () => {
   return (
     <InfoBlock title="Условия доставки">
       <p>
-        Доставляем по четвергам и воскресеньям в&nbsp;пределах Екатеринбурга.
+        Доставляем по понедельникам и четвергам в&nbsp;пределах Екатеринбурга.
         Стоимость доставки&nbsp;-&nbsp;{config.deliveryPrice}&nbsp;₽.
         При&nbsp;заказе от&nbsp;{config.freeDeliveryFrom}&nbsp;₽ доставка будет
         бесплатной.
@@ -14,7 +14,7 @@ export const DeliveryInfo = () => {
       <p>
         По индивидуальной договорённости доставим в&nbsp;коттеджные посёлки
         и&nbsp;города-спутники в&nbsp;радиусе 20&nbsp;км. Стоимость доставки
-        составит {config.deliveryPrice}&nbsp;₽.
+        составит от {config.farDeliveryPrice}&nbsp;₽ (в зависимости от удалённости).
       </p>
     </InfoBlock>
   );
