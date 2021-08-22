08c47b9e42ce7c5f45c7ba5a4086bf2bc9a89595
Arkady Anderjushkin <edheltur@yandex.ru>
2021-08-22 23:54:34 +0500

    Фикс билда

--- pages/admin/empty-images.ts
+++
@@ -1,8 +0,0 @@
-import { EmptyImagesPage } from "components/EmptyImagesPage";
-import { GetServerSideProps } from "next";
-
-export default EmptyImagesPage;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return { props: {} };
-};
